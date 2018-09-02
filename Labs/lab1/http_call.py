a3713f5a31835023da94a9e1ebb11cd6171a6f7f
varunu28 <varun.u28@gmail.com>
2018-09-01 20:52:58 -0700

    Updated lab1 code

--- Labs/lab1/http_call.py
+++ Labs/lab1/http_call.py
@@ -1,14 +1,15 @@
 import requests
 
-def make_request():
+def main():
     url = "https://webhook.site/817fd522-e7fb-4916-a41f-acbc0db04371"
     
-    response = requests.get(url)
-    print(response.headers['Date'])
-
-def main():
-    for i in range(3):
-        make_request()
+    response1 = requests.get(url)
+    response2 = requests.get(url)
+    response3 = requests.get(url)
+    
+    print(response1.headers['Date'])
+    print(response2.headers['Date'])
+    print(response3.headers['Date'])
 
 if __name__ == "__main__":
     main()
